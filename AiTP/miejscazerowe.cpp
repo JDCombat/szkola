e56df1b6c7f2983514b3136a06787f1b288fdb6a
JDCombat <bartekkozub2007@gmail.com>
2023-03-02 16:07:05 +0100

    AiTP 02.03 - miejsca zerowe

--- AiTP/miejscazerowe.cpp
+++ AiTP/miejscazerowe.cpp
@@ -1,20 +1,59 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double funkcja(double x){
-    return pow(x-5, 3);
+double f(double x){
+    return cos(x-1) - 3*x + 2;
 }
-double miejsceZerowe(double a, double b){
-    float e = 0.001f;
-    float c = (a+b)/2;
-    if (funkcja(c) * funkcja(a) < 0)
-        b = c;
-    else
-        a = c;
-    if (fabs(a-b) < e && fabs(funkcja(a)-funkcja(b)) < e)
+double bisekcja_rek(double a, double b){
+    double c = (a+b)/2;
+    float e = 0.000001;
+    if(fabs(a-b)>=e || fabs(f(b) - f(a)) >= e){
+        if((f(a) * f(c)) <= 0){
+            return bisekcja_rek(a, c);
+        }
+        else{
+            return bisekcja_rek(c, b);
+        }
+    }
+    else{
         return c;
-    miejsceZerowe(a, b);
+    }
 }
+
+double bisekcja_it(double a, double b){
+    double e = 0.000001;
+    double c;
+    while(fabs(a-b)>=e || fabs(f(b) - f(a)) >= e){
+        c = (a+b)/2;
+
+        if((f(a) * f(c)) <= 0){
+            b = c;
+        }
+        else{
+            a = c;
+        }
+    }
+    return c;
+}
+
+double fals(double a, double b, double epsilon){
+    double c = a - f(a)*((b-a)/(f(b) - f(a)));
+    while(fabs(f(c)) > epsilon){
+        if (f(a)*f(c) < 0){
+            b = c;
+        }
+        else{
+            a = c;
+        }
+        c = a - f(a)*((b-a)/(f(b) - f(a)));
+    }
+    return c;
+}
+
 int main(){
-    cout << miejsceZerowe(-5, 10);
+    double e = 0.0000000001;
+    cout << bisekcja_rek(0,2) << endl << endl;
+    cout << bisekcja_it(0,2) << endl << endl;
+
+    cout << fals(-1000, 1000, e);
     }
