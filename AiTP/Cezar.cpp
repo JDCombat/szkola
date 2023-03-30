492a97fb5f5fe4deebc7f8f849bf30e742583ca5
JDCombat <bartekkozub2007@gmail.com>
2023-03-30 15:50:41 +0200

    AiTP 16.03 - cezar

---
+++ AiTP/Cezar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string alfabet = "ABCDEFG";
+    string szyfr = "elo zelo";
+    int k = 3;
+    for(int n=0;n<szyfr.length(); n++){
+        for(int l = 0;l<szyfr.length();l++){
+
+        }
+        szyfr[n] = alfabet[i+k];
+    }
+}
+//
+// Created by Kozub Bartosz on 30.03.2023.
+//
