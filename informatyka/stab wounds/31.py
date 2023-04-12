a4d002956e2f57a66cf83f6522aeaa9af88f0fa9
JDCombat <bartekkozub2007@gmail.com>
2023-04-12 10:57:53 +0200

    informat 12.04

--- informatyka/stab wounds/31.py
+++ informatyka/stab wounds/31.py
@@ -1,4 +1,18 @@
-def w(p, k):
-    return pow((p/100), k)
+def fuu(p, k):
+    w = pow((p/100), k)
 
-if 
+    if w >= 0.4 and w < 0.5:
+        return 2
+    elif w >= 0.5 and w < 0.75:
+        return 3
+    elif w>= 0.75 and w< 0.85:
+        return 4
+    elif w>= 0.85 and w < 0.95:
+        return 5
+    elif w>=0.95:
+        return 6
+
+print(fuu(20,0.5), end=";")
+print(fuu(31, 0.3), end=";")
+print(fuu(49,0.2), end=";")
+print(fuu(89,1.5))
