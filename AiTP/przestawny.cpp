c727a651e1794a8cdf4a53bca97f95dd15137b93
JDCombat <bartekkozub2007@gmai.com>
2023-05-25 14:30:47 +0200

    Add files

---
+++ AiTP/przestawny.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+string przestawieniowy(string wyraz, int tab[]);
+int main(){
+
+}
+
+string przestawieniowy(string wyraz, int tab[]){
+
+
+}
