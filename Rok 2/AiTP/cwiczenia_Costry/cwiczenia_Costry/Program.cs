83985def41405ee4968999c9823990c93291d6fe
JDCombat <bartekkozub2007@gmail.com>
2023-11-28 22:24:44 +0100

    idę spać a nie

--- Rok 2/AiTP/cwiczenia_Costry/cwiczenia_Costry/Program.cs
+++ Rok 2/AiTP/cwiczenia_Costry/cwiczenia_Costry/Program.cs
@@ -17,6 +17,7 @@ namespace Aitp_excercises
             Console.WriteLine(Math.Round(first * second, 2));
             Console.WriteLine(Math.Round(first / second, 2));
         }
+        
 
         static void cw1star()
         {
@@ -67,13 +68,62 @@ namespace Aitp_excercises
             string[] s = Regex.Split(input, "([a-zA-Z0-9]+)");
             Console.WriteLine(s[3] + s[2] + s[1]);
         }
+
+        static void cw4()
+        {
+            uint number = 0;
+            number = uint.Parse(Console.ReadLine());
+            string num_str = number.ToString();
+
+        }
+
+        static double[] cw6()
+        {
+            Random rnd = new Random();
+            double[] tab = new double[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tab[i] = rnd.Next(10);
+                Console.Write(tab[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < tab.Length - 1; i++)
+            {
+                for (int j = 0; j < tab.Length - i - 1; j++)
+                {
+                    if (tab[j] > tab[j + 1])
+                    {
+                        (tab[j], tab[j + 1]) = (tab[j + 1], tab[j]);
+                    }
+                }
+            }
+            return tab;
+        }
+
+        static uint nwd(uint a, uint b)
+        {
+            return b == 0 ? a : nwd(b, a % b);
+        }
+        static double cw8()
+        {
+            uint a, b;
+            a = uint.Parse(Console.ReadLine());
+            b = uint.Parse(Console.ReadLine());
+            return nwd(a, b);
+        }
         public static void Main(String[] args)
         {
             // cw1();
             // cw1star();
             // Console.WriteLine(cw2() == true ? "Yesh" : "no");
             // Console.WriteLine(cw2star() == true ? "Yesh" : "no");
-            cw3();
+            // cw3();
+            
+            // foreach(var item in cw6())
+            // {
+            //     Console.Write(item + " ");
+            // }
+            cw8();
         }
     }
 }
